b2e4768babf48ba73a342c261cdb5e8d62d1f9e1
xoingyifan <xyf0728@126.com>
2017-12-27 09:46:15 +0800

    assignment1-q2_sigmoid

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,8 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1 / (1 + np.exp(-x))
+    # s = 1.0 / (1.0 + np.exp(-x))  # 1 and 1.0 are same or not?.
     ### END YOUR CODE
 
     return s
@@ -35,7 +36,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = s * (1 - s)
     ### END YOUR CODE
 
     return ds
@@ -72,10 +73,10 @@ def test_sigmoid():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    # raise NotImplementedError
     ### END YOUR CODE
 
 
 if __name__ == "__main__":
-    test_sigmoid_basic();
+    test_sigmoid_basic()
     test_sigmoid()
